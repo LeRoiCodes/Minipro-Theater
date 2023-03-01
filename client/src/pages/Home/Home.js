7a070319b8f0233a2024b6d06a20c38e09f08e6d
LeRoi Vladimir <banyamkimbi@vrfound.org>
2023-03-01 06:31:11 +0100

    now showing section 1
    working on now showing section

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -4,13 +4,30 @@ import img2 from '../../images/Popcorn with soda pop cinematic clapper and 3D gl
 
 function Home() {
   return (
-    <div className='intro'>
+    <div className='home'>
+    <section className='intro'>
       <div className='intro_text'>
         <h1>MOVIE TICKET BOOKING</h1>
         <p>Get your movie ticket, your favorite movie snacks and reserve your preferred seat at your convinience.</p>
-        <a className='btn_red'>GET STARTED</a>
+        <a className='btn_red' href='#'>GET STARTED</a>
       </div>
       <img src={img2} alt="popcorn"/>
+    </section>
+    <hr />
+    <section className='showing'>
+      <h2>Now Showing</h2>
+      <div className='show_container'>
+        <div className='show_card'>
+          <img />
+          <div className='show_text'>
+            <div>
+              <h4>Happy halloween</h4>
+            </div>
+            <a className='btn_red'>BOOK</a>
+          </div>
+        </div>
+      </div>
+    </section>
     </div>
   )
 }
